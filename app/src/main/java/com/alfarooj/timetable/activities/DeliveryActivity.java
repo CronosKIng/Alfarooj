df3b7248be67fed1d23e538eb3ee4db7963147f4
CronosKIng <ismailyussuf33@gmail.com>
2026-04-12 12:52:23 +0300

    Convert DeliveryActivity to menu-based UI - remove all buttons, add delivery_menu.xml

--- app/src/main/java/com/alfarooj/timetable/activities/DeliveryActivity.java
+++ app/src/main/java/com/alfarooj/timetable/activities/DeliveryActivity.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
 import android.os.Looper;
 import android.view.Menu;
@@ -13,6 +12,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -33,7 +33,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class DeliveryActivity extends BaseActivity {
-    
+
     private TextView tvWelcome, tvStatus;
     private SessionManager session;
     private FusedLocationProviderClient fusedLocationClient;
@@ -46,30 +46,34 @@ public class DeliveryActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery);
-        
+
+        // Setup Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         session = new SessionManager(this);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        
+
         tvWelcome = findViewById(R.id.tvWelcome);
         tvStatus = findViewById(R.id.tvStatus);
-        
+
         updateUI();
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST);
         }
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.delivery_menu, menu);
         return true;
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        
+
         if (id == R.id.action_pickup) {
             checkLocation(TranslationHelper.translateTextDirect("pickup_order"), "Pickup Order");
             return true;
@@ -98,7 +102,7 @@ public class DeliveryActivity extends BaseActivity {
             showLanguageDialog();
             return true;
         }
-        
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -135,16 +139,16 @@ public class DeliveryActivity extends BaseActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(TranslationHelper.translateTextDirect("Are you late?"));
         builder.setMessage(TranslationHelper.translateTextDirect("Did you arrive late to work today?"));
-        
+
         builder.setPositiveButton(TranslationHelper.translateTextDirect("Yes, I'm late"), (dialog, which) -> {
             showCommentDialog();
         });
-        
+
         builder.setNegativeButton(TranslationHelper.translateTextDirect("No, I'm on time"), (dialog, which) -> {
             pendingComment = "";
             startLocationCheck();
         });
-        
+
         builder.show();
     }
 
@@ -155,7 +159,7 @@ public class DeliveryActivity extends BaseActivity {
         final EditText input = new EditText(this);
         input.setHint(TranslationHelper.translateTextDirect("Enter your reason here..."));
         builder.setView(input);
-        
+
         builder.setPositiveButton(TranslationHelper.translateTextDirect("Submit"), (dialog, which) -> {
             pendingComment = input.getText().toString().trim();
             startLocationCheck();
@@ -198,7 +202,7 @@ public class DeliveryActivity extends BaseActivity {
                 else
                     tvStatus.setText(TranslationHelper.translateTextDirect("Not at work location!"));
             }
-            
+
             @Override
             public void onFailure(Call<LocationResponse> call, Throwable t) {
                 tvStatus.setText(TranslationHelper.translateTextDirect("Network error"));
@@ -210,7 +214,7 @@ public class DeliveryActivity extends BaseActivity {
         String location = "Lat: " + currentLatitude + ", Lon: " + currentLongitude;
         AttendanceRequest request = new AttendanceRequest(session.getUserId(), session.getUsername(), session.getFullName(),
                 session.getDepartment(), pendingEventType, pendingEventName, currentLatitude, currentLongitude, location);
-        
+
         if (pendingEventType.equals(TranslationHelper.translateTextDirect("sign_in")) && !pendingComment.isEmpty()) {
             request.setComment(pendingComment);
         }
@@ -220,7 +224,7 @@ public class DeliveryActivity extends BaseActivity {
         } else if (pendingEventType.equals(TranslationHelper.translateTextDirect("dropoff_order"))) {
             request.setOrderType(TranslationHelper.translateTextDirect("dropoff"));
         }
-        
+
         ApiClient.getApiService().recordAttendance(request).enqueue(new Callback<AttendanceResponse>() {
             @Override
             public void onResponse(Call<AttendanceResponse> call, Response<AttendanceResponse> response) {
@@ -232,7 +236,7 @@ public class DeliveryActivity extends BaseActivity {
                     tvStatus.setText(TranslationHelper.translateTextDirect("Failed"));
                 }
             }
-            
+
             @Override
             public void onFailure(Call<AttendanceResponse> call, Throwable t) {
                 tvStatus.setText(TranslationHelper.translateTextDirect("Network error"));
